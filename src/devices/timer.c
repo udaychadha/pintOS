217d3294645eed4d9617505496ec4cd872cd8599
Uday Chadha <uchadha@usc.edu>
2016-07-02 10:59:40 -0700

    all alarm tests passed!(except the one with PD)  :)

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -39,6 +39,7 @@ timer_init (void)
 {
   pit_configure_channel (0, 2, TIMER_FREQ);
   intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+  list_init(&sleeping_list);
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
